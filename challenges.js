f6816bc8ef42e2d0d2383864d54df036beb8d56f
iaroslavnikitin <nikitin.iaroslav@gmail.com>
2021-03-10 15:33:06 -0800

    03102021_2nd_commit

--- challenges.js
+++ challenges.js
@@ -1311,13 +1311,12 @@ console.log(reverse('nikitin yaro')); // oray nitikin
 
 // #83. Prime number
 function isPrime(num) {
-    if (num < 2) return false;
-    else if (num != 2 && num % 2 === 0) return false;
-    let sqrt = Math.sqrt(num);
-    for (let i = 3; i <= sqrt; i += 2) {
+    if (num < 2 || num != 2 && num % 2 === 0) return false;
+    for (let i = 3; i <= Math.sqrt(num); i += 2)
         if (num % i === 0) return false;
-    }
     return true;
 }
+console.log(isPrime(5)); // true
 console.log(isPrime(2)); // true
+console.log(isPrime(1.5)); // false
 console.log(isPrime(1)); // false
