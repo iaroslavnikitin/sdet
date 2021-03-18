eb56dc9acf03dc6793d1bfba6770245dfaa582ad
iaroslavnikitin <nikitin.iaroslav@gmail.com>
2021-03-18 00:10:02 -0700

    03182021_1st_commit

--- challenges.js
+++ challenges.js
@@ -1523,3 +1523,24 @@ console.log(distanceComparison(1, -8, -6, 3));    // должна возврат
 console.log(distanceComparison(-10, -2, -7, -5)); // должна возвратить 2
 console.log(distanceComparison(-1, 4, -2, -3));   // должна возвратить 2
 console.log(distanceComparison(2, 5, -1, 6));     // должна возвратить 1
+
+// #96. Holiday VI - Shark Pontoon
+// You are given 5 variables;
+// sharkDistance = distance from the shark to the pontoon. The shark will eat you if it reaches you before you escape to the pontoon.
+// sharkSpeed = how fast it can move in metres/second.
+// pontoonDistance = how far you need to swim to safety in metres.
+// youSpeed = how fast you can swim in metres/second.
+// dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
+// The pontoon, you, and the shark are all aligned in one dimension.
+// If you make it, return "Alive!", if not, return "Shark Bait!".
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) {
+  if (dolphin) sharkSpeed /= 2;
+  return ((pontoonDistance / youSpeed) < (sharkDistance / sharkSpeed)) ? "Alive!" : "Shark Bait!";
+}
+console.log(shark(12, 50, 4, 8, true)); // Alive!
+console.log(shark(7, 55, 4, 16, true)); // Alive!
+console.log(shark(24, 0, 4, 8, true));  // Shark Bait!
+console.log(shark(10, 8, 3, 7, true));  // Shark Bait!
+console.log(shark(45, 159, 0, 0, true)); // Alive!
+console.log(shark(6, 136, 1, 16, true)); // Alive!
+console.log(shark(17, 123, 4, 16, false)); // Alive!
