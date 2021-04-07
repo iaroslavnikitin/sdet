91dba1adddaa41393b09af0d0b535190e59526ef
iaroslavnikitin <nikitin.iaroslav@gmail.com>
2021-04-06 21:11:52 -0700

    05062021_2nd_commit

--- challenges.js
+++ challenges.js
@@ -2487,3 +2487,39 @@ console.log(solve("code")); // "code"
 console.log(solve("CODe")); // "CODE"
 console.log(solve("COde")); // "code"
 console.log(solve("Code")); // "code"
+
+// # 143, The Wide-Mouthed frog!
+// The wide mouth frog is particularly interested in the eating habits of other creatures.
+// He just can't stop asking the creatures he encounters what they like to eat. But then he meet the alligator who just LOVES to eat wide-mouthed frogs!
+// When he meets the alligator, it then makes a tiny mouth.
+// Your goal in this kata is to create complete the mouth_size method this method take one argument animal which corresponds to the animal encountered by frog.
+// If this one is an alligator (case insensitive) return small otherwise return wide.
+function mouthSize(animal) {
+  return animal.toLowerCase() === 'alligator' ? 'small' : 'wide';
+}
+console.log(mouthSize("toucan")); // 'wide'
+console.log(mouthSize("ant bear")); // 'wide'
+console.log(mouthSize("alligator")); // 'small'
+
+// # 144, Get the Middle Character
+// You are going to be given a word. Your job is to return the middle character of the word.
+// If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+// Examples:
+// Kata.getMiddle("test") should return "es"
+// Kata.getMiddle("testing") should return "t"
+// Kata.getMiddle("middle") should return "dd"
+// Kata.getMiddle("A") should return "A"
+// Input:
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases).
+// You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+// Output:
+// The middle character(s) of the word represented as a string.
+function getMiddle(s) {
+  if (s.length % 2 === 0) return s[s.length / 2 - 1] + s[s.length / 2];
+  else if (s.length % 2 !== 0) return s[s.length / 2 - 0.5];
+  else return s;
+}
+console.log(getMiddle("test")); // 'es'
+console.log(getMiddle("testing")); // 't'
+console.log(getMiddle("middle")); // 'dd'
+console.log(getMiddle("A")); // 'A'
