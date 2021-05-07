7daf0615ae398d562f651c96a412906482cc4a69
iaroslavnikitin <nikitin.iaroslav@gmail.com>
2021-05-07 12:52:22 -0700

    05072021_2тв_сщььше

--- JS_practice_10_code_challenges.js
+++ JS_practice_10_code_challenges.js
@@ -1,30 +1,30 @@
-// let sq = '';
-// for (let i = 0; i < 4; i++) {
-//     for (let j = 0; j < 4; j++) sq += '*  ';
-//     sq += '\n';
-// }
-// console.log(sq);
+let sq = '';
+for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) sq += '*  ';
+    sq += '\n';
+}
+console.log(sq);
 
-// let sq = '';
-// for (let i = 0; i < 5; i++) {
-//     for (let j = 0; j <= i; j++) sq += '* ';
-//     sq += '\n';
-// }
-// console.log(sq);
+let sq = '';
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j <= i; j++) sq += '* ';
+    sq += '\n';
+}
+console.log(sq);
 
-// let favColor = 'red';
-// switch (favColor) {
-//     case 'blue' : console.log('Violets are blue'); break;
-//     case 'red' : console.log('Roses are red'); break;
-//     default : console.log('Sugar is sweet'); break;
-// }
+let favColor = 'red';
+switch (favColor) {
+    case 'blue' : console.log('Violets are blue'); break;
+    case 'red' : console.log('Roses are red'); break;
+    default : console.log('Sugar is sweet'); break;
+}
 
-// let pay = prompt('the amount you\'re paying?');
-// let owe = prompt('how much we owe you?');
-// function task(pay, owe) {
-//     if (pay === owe) return `You gave the exact amount.`;
-//     else if (pay > owe) return `You gave too much. Your change due is $${pay - owe}.`;
-//     else if (pay < owe) return 'You gave too little. You still owe $' + Math.abs(owe - pay) + '.';
-// }
-// console.log(task(pay, owe));
+let pay = prompt('the amount you\'re paying?');
+let owe = prompt('how much we owe you?');
+function task(pay, owe) {
+    if (pay === owe) return `You gave the exact amount.`;
+    else if (pay > owe) return `You gave too much. Your change due is $${pay - owe}.`;
+    else if (pay < owe) return 'You gave too little. You still owe $' + Math.abs(owe - pay) + '.';
+}
+console.log(task(pay, owe));
 
